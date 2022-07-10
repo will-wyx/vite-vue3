454d80a619f240f5aa9e4344347f50925034b18c
will-wyx <wyx_msn@hotmail.com>
2022-07-10 22:57:36 +0800

    feat(*): 集成乾坤

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,37 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
+import {qiankunWindow, renderWithQiankun, QiankunProps} from "vite-plugin-qiankun/es/helper";
 
-createApp(App).mount('#app')
+let instance = null;
+
+function render(props = {}) {
+    const {container} = props;
+    console.log(container)
+    instance = createApp(App)
+        // .mount('#app')
+        .mount(container ? container.querySelector('#app') : document.getElementById("app"))
+}
+
+
+renderWithQiankun({
+    bootstrap(): void | Promise<void> {
+        return undefined;
+    },
+    update(props: QiankunProps): void | Promise<void> {
+        return undefined;
+    },
+    mount(props) {
+        render(props)
+    },
+    unmount(props: any) {
+        if (instance) {
+            instance.unmount()
+            instance._container.innerHTML = '';
+            instance = null;
+        }
+    }
+})
+
+if (!qiankunWindow.__POWERED_BY_QIANKUN__) {
+    render()
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import qiankun from 'vite-plugin-qiankun'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [
+        vue(),
+        qiankun(
+            'vue3-vite',
+            {
+                useDevMode: true
+            }
+        )
+    ],
     server: {
         port: 3009,
     }
